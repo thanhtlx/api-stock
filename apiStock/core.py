02063d18750945982214c3efcefae2f1e0e5ad2a
thanhtlx <thanh1908.xx@gmail.com>
2022-03-01 10:07:57 +0700

    add new resource

--- apiStock/core.py
+++ apiStock/core.py
@@ -1,8 +1,11 @@
 import requests
 import random
+from datetime import datetime
 from .const import USER_AGENTS
 
+
 URL = 'https://finfo-api.vndirect.com.vn/v4/stock_prices?sort=date&q=code:%s~date:gte:%s~date:lte:%s&size=100000'
+URL_SSI = "https://iboard.ssi.com.vn/dchart/api/history?resolution=D&symbol=%s&from=%s&to=%s" 
 def DICT_FILTER(x, y): return dict([(i, x[i]) for i in x if i in set(y)])
 
 
@@ -10,6 +13,25 @@ KEYS = ['date', 'ceilingPrice', 'floorPrice', 'open',
         'high', 'low', 'close', 'average', 'change',
         "nmVolume"]
 
+
+
+def getStockHistoryV2(code,start_date,end_date):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    end_time = int(end_date.timestamp())
+    start_time = int(start_date.timestamp())
+    try:
+        r = requests.get(URL_SSI % (code, start_time, end_time),
+                         headers={"USER-AGENT": USER_AGENTS[random.randint(0, len(USER_AGENTS)-1)]})
+        res = r.json()
+    except Exception as e:
+        return []
+    result = dict()
+    for k,v in res.copy().items():
+        if isinstance(v, list):
+            v.reverse()
+            result[k] = v
+    return result
 
 def getStockHistory(code, start_date, end_date):
     try:
--- apiStock/test.py
+++ apiStock/test.py
@@ -1,5 +1,7 @@
-from core import getStockHistory
+from core import getStockHistoryV2,getStockHistory
 
 if __name__ == "__main__":
-    s = getStockHistory("BID", "2022-01-11", "2022-02-12")
-    print(s)
+    s = getStockHistory("PAN", "2000-01-11", "2022-02-12")
+    s1 = getStockHistoryV2("PAN", "2000-01-11", "2022-02-12")
+    print(len(s))
+    print(len(s1["v"]))
